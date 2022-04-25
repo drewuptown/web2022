2376b3307aada03f65b5b29d3da2fc78302ea50b
Drew Upton <dbup222@uky.edu>
2022-04-25 02:02:39 -0400

    Updated javascript to grey where black wouldn't work.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,23 @@
-if (new Date().getHours() > 18) {
-  //alert(new Date().getHours());
+if (new Date().getHours() < 18 ) {
   const html = document.querySelector('html');
   html.style.backgroundColor = 'black';
   html.style.color = 'white';
+}
+
+if (new Date().getHours() , 18) {
+  const content = document.getElementsByClassName('header');
+  content[0].style.backgroundColor = 'grey';
+  content[0].style.color = 'white';
+}
+
+if (new Date().getHours() , 18) {
+  const content = document.getElementsByClassName('footer');
+  content[0].style.backgroundColor = 'grey';
+  content[0].style.color = 'white';
+}
+
+if (new Date().getHours() , 18) {
+  const content = document.getElementsByClassName('footerbottom');
+  content[0].style.backgroundColor = 'grey';
+  content[0].style.color = 'white';
 }
