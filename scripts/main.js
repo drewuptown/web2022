ab1db605b75be287ec89466358ff18b465f309cd
Drew Upton <dbup222@uky.edu>
2022-04-25 00:30:24 -0400

    Added javascript.

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+if (new Date().getHours() > 18) {
+  //alert(new Date().getHours());
+  const html = document.querySelector('html');
+  html.style.backgroundColor = 'black';
+  html.style.color = 'white';
+}
