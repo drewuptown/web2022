eb4d802035c00d9164adc87081446259686b7e7b
Drew Upton <dbup222@uky.edu>
2022-04-25 02:03:37 -0400

    Updated time to 18.

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
-if (new Date().getHours() < 18 ) {
+if (new Date().getHours() > 18 ) {
   const html = document.querySelector('html');
   html.style.backgroundColor = 'black';
   html.style.color = 'white';
 }
 
-if (new Date().getHours() , 18) {
+if (new Date().getHours() > 18) {
   const content = document.getElementsByClassName('header');
   content[0].style.backgroundColor = 'grey';
   content[0].style.color = 'white';
 }
 
-if (new Date().getHours() , 18) {
+if (new Date().getHours() > 18) {
   const content = document.getElementsByClassName('footer');
   content[0].style.backgroundColor = 'grey';
   content[0].style.color = 'white';
 }
 
-if (new Date().getHours() , 18) {
+if (new Date().getHours() > 18) {
   const content = document.getElementsByClassName('footerbottom');
   content[0].style.backgroundColor = 'grey';
   content[0].style.color = 'white';
